9bde1e00543451d473d082a1cd741c5018c37d43
Anh Vu <anhvu@anhvu.attlocal.net>
2023-10-27 23:13:46 -0500

    add only manager can create new employee

---
+++ frontend/src/main_page/manager_mainpage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { Button } from 'react-bootstrap';
+
+const EmployeeMainPage = () => {
+    return (
+        <div>
+            <h1>Welcome Back</h1>
+            <div>
+                <Button variant="primary" as={Link} to={"/packages"}>Click here to check for existing packages in all system</Button>
+            </div>
+            <div>
+                <Button variant="primary" as={Link} to={"/employee_signup"}>Click here to sign up a new employee</Button>
+            </div>
+        </div>
+    );
+}
+
+export default EmployeeMainPage;
